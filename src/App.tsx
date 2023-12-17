549cf2d5933984b18c60f427cb7bcda617f2f2d9
Roshan Gamage <apple.roshangamage@gmail.com>
2023-12-17 22:28:26 +0530

    add advance table component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import { DefaultTable } from "./components"
+import AdvanceTable from "./components/AdvanceTable/AdvanceTable"
 
 export default function App() {
     return (
         <>
             <section className="p-5 h-screen">
                 <DefaultTable tableDataSet={{data: [{"id": 1, "name": "test", "mail" : "mail@m.com"}]}} />
+                <AdvanceTable 
+                    tableDataSet={{
+                        data: [{"id": 1, "name": "test", "mail" : "mail@m.com"}],
+                        dataCount: 1,
+                        currentPaginationIndex: 1,
+                        dataPerPage: 10
+                    }}
+                    
+                    
+                />
             </section>   
         </>
     )
